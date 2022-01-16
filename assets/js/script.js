21b6675d0cf77a0c250ca4b73e096e2eb0d10ad8
Baofeng Guo <peterguo1983@gmail.com>
2022-01-16 13:16:06 -0800

    Tailwindcss group list styling to the latest news section.

--- assets/js/script.js
+++ assets/js/script.js
@@ -381,9 +381,9 @@ fetch(newsQueryUrl)
   for (var i = 0; i < newsData.response.docs.length; i++) {
     var newsUrl = newsData.response.docs[i].web_url;
     var newsTitle = newsData.response.docs[i].abstract;
-    //Use tailwind group list: https://flowbite.com/docs/components/list-group/
+    //Use tailwindcss group list: https://flowbite.com/docs/components/list-group/
     let latestNewsEl = $('<li>');
-    latestNewsEl.append([i+1],". <a href='" + newsUrl + "' target='_blank'>" + newsTitle + "</a>");
+    latestNewsEl.addClass("block py-2 px-4 w-full border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white").append([i+1],". <a href='" + newsUrl + "' target='_blank'>" + newsTitle + "</a>");
     latestNewsEl.append($('<br>'));
     newsListEl.append(latestNewsEl);
 }
